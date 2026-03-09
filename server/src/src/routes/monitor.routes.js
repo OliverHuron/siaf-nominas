9ac95483ebba2a2c96b1f4fdc48644d6dec527a8
OliverHuron <oliverhuron@gmail.com>
2026-03-09 00:43:15 -0600

    fix: remove duplicate src/src directory

--- server/src/src/routes/monitor.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const monitorController = require('../controllers/monitor.controller');
-const { authenticateToken } = require('../middleware/auth.middleware');
-
-router.get('/metrics', authenticateToken, monitorController.getMetrics);
-router.get('/processes', authenticateToken, monitorController.getProcesses);
-
-module.exports = router;
